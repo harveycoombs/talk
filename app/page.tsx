e1ca5cd4729706076120f047c39a69e385a15fc3
harveycoombs <harveycoombs23@outlook.com>
2024-12-16 18:11:17 +0000

    Button appearance change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Button from "./components/button";
+
 export default function Home() {
     return (
-        <main></main>
+        <main className="grid place-items-center h-screen">
+            <Button>Click Me</Button>
+        </main>
     );
 }
