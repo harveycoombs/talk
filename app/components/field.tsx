b2e8f249065a463e66c39f4910b452d0e2add3b8
harveycoombs <harveycoombs23@outlook.com>
2024-12-16 18:13:37 +0000

    Field change

--- app/components/field.tsx
+++ app/components/field.tsx
@@ -7,6 +7,6 @@ interface Properties {
 }
 
 export default function Field({ type, classes, error, warning, ...rest }: Properties) {
-    let classList = `px-4 py-3 rounded-lg box-border text-[0.8rem] border border-slate-300 bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-sky-500 focus:shadow-md ${classes?.length ? " " + classes : ""}`;
+    let classList = `px-4 py-3 rounded-lg box-border text-[0.8rem] border border-slate-300 bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-slate-800 focus:shadow-md ${classes?.length ? " " + classes : ""}`;
     return <input type={type ?? "text"} className={classList} {...rest} />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Field from "./components/field";
 import Button from "./components/button";
 
 export default function Home() {
     return (
         <main className="grid place-items-center h-screen">
+            <Field />
             <Button>Click Me</Button>
         </main>
     );
