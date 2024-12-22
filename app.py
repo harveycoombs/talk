e08d676d169c39fa441ffde45c24b95edfffc151
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-22 22:29:27 +0000

    Initial

---
+++ app.py
@@ -0,0 +1,13 @@
+# Talk (WIP) ~ Harvey Coombs, 2024
+import ollama
+
+model_name = input("Model Name (Default = DeepSeek V2 Lite): ")
+model = ollama.load_model("deepseek-v2:lite" if len(model_name) == 0 else model_name)
+
+prompt = input(">> ")
+
+output = model.predict({
+    'text': prompt
+})
+
+print(output)
